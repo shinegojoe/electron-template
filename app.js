025ab64738e5fdebaddebf270a52262896c2d4e4
taka <shinegojoe@gmail.com>
2023-01-12 06:41:19 -0800

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+const endPoint = require('./dist/out-tsc/electron/entryPoint');
+
+const { app, BrowserWindow, BrowserView, ipcMain, nativeTheme } = require('electron')
+const url = require("url");
+const path = require("path");
+
+// require('./controller/stockController.js');
+// require('./controller/pointController.js');
+
+// const stockDao = require('./service/stockDao.js');
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({ width: 800, height: 600,  webPreferences: {
+    nodeIntegration: true,
+    enableRemoteModule: true, 
+    contextIsolation: false
+  } })
+
+  const view = new BrowserView()
+  win.setBrowserView(view)
+  view.setBounds({ x: 0, y: 0, width: 800, height: 600 })
+  // view.setAutoResize();
+  // view.webContents.loadURL('https://electronjs.org')
+  const file = path.join(__dirname, `/dist/electron/index.html`)
+  console.log("file: ", file);
+  // view.webContents.loadFile(file);
+  // win.loadFile(file);
+  win.loadURL('http://localhost:4200')
+
+  win.webContents.openDevTools()
+
+})
+
+nativeTheme.themeSource = 'dark'
